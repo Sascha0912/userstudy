fc2e3d4294258244031a8114104c47628340f4e8
Sascha Obst <s.obst.so@live.de>
2023-04-11 12:54:50 +0200

    fixes

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+import json
+import mariadb
 
 app = Flask(__name__)
 
+
+def check_user():
+    mariadb.connect(
+        host='85.214.159.31',
+        port=3306,
+        user='sascha',
+        password='',
+        database='ux_tests'
+    )
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -16,6 +27,26 @@ def studyu():
 @app.route('/userstudy/1_0')
 def study1_0():
     return render_template('userstudy1_0.html')
+
+
+@app.route('/userstudy/2_0')
+def study2_0():
+    return render_template('userstudy2_0.html')
+
+
+@app.route('/userstudy/3_0')
+def study3_0():
+    return render_template('userstudy3_0.html')
+
+
+@app.route('/userstudy/4_0')
+def study4_0():
+    return render_template('userstudy4_0.html')
+
+
+@app.route('/userstudy/5_0')
+def study5_0():
+    return render_template('userstudy5_0.html')
 
 
 if __name__ == '__main__':
